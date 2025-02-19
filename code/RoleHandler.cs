6293b78cbe976d0d66266e1d75653b71ce59d230
Titanovsky <titanovskyteam@gmail.com>
2025-02-19 07:38:17 +0500

    add raw RoleHandler

---
+++ code/RoleHandler.cs
@@ -0,0 +1,21 @@
+﻿public class RoleHandler
+{
+	public static List<string> Roles { get; private set; } = new();
+
+	public void Add(string role)
+	{
+		Roles.Add(role);
+	}
+
+	public void Remove(string role)
+	{
+		Roles.Remove(role);
+	}
+
+	public string Get( string role )
+	{
+		if (!Roles.Contains( role )) return null;
+
+		return role;
+	}
+}
