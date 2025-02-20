905abafc4cb5db1acc9bf8a8c58723f8d64afc08
Titanovsky <titanovskyteam@gmail.com>
2025-02-20 08:41:00 +0500

    realize IValid for Role

--- code/Role.cs
+++ code/Role.cs
@@ -1,7 +1,9 @@
-﻿public class Role
+﻿public class Role : IValid
 {
 	public string Id { get; set; }
 	public string Name { get; set; }
+
+	public bool IsValid => this != null;
 
 	public Role(string id, string name = "")
 	{
