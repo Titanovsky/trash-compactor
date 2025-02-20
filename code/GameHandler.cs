5cc0c33142357dcd2f6dd8594b1b28eaf496c217
Titanovsky <titanovskyteam@gmail.com>
2025-02-20 09:57:08 +0500

    simple init

--- code/GameHandler.cs
+++ code/GameHandler.cs
@@ -4,15 +4,35 @@ public static class GameHandler
 	{
 		InitRoles();
 		//InitTrashTrigger();
-		//SpawnPlayer();
+		SpawnPlayer();
 	}
 
 	private static void InitRoles()
 	{
 		RoleHandler.Reset(); // cuz static
 
-		RoleHandler.Add( new Role( "spectator", "Spectator" ) );
-		RoleHandler.Add( new Role( "trashman", "Trashman" ) );
-		RoleHandler.Add( new Role( "soccer", "Soccer" ) );
+		string spectator = GameRoles.Spectator.ToString().ToLower();
+		RoleHandler.Add( new Role( spectator, "Spectator" ) );
+
+		string trashman = GameRoles.Trashman.ToString().ToLower();
+		RoleHandler.Add( new Role( trashman, "Trashman" ) );
+
+		string soccer = GameRoles.Soccer.ToString().ToLower();
+		RoleHandler.Add( new Role( soccer, "Soccer" ) );
 	}
+
+	private static void SpawnPlayer()
+	{
+		var ply = Player.Instance;
+
+		ply.SetRole( "soccer" );
+		ply.SetupRole();
+	}
+}
+
+public enum GameRoles
+{
+	Spectator,
+	Trashman,
+	Soccer
 }
--- code/Player.cs
+++ code/Player.cs
@@ -4,6 +4,8 @@ public sealed class Player : Component, Component.IDamageable
 {
 	public static Player Instance { get; set; }
 
+	public Role Role { get; set; }
+
 	protected override void OnAwake()
 	{
 		if ( Instance.IsValid() ) return;
@@ -14,5 +16,40 @@ public sealed class Player : Component, Component.IDamageable
 	public void OnDamage( in DamageInfo damage )
 	{
 		Log.Info( damage.Attacker );
+	}
+
+	public void SetRole( Role role )
+	{
+		if (!role.IsValid()) return;
+
+		Role = role;
+
+		Log.Info( $"The Player {GameObject} has the role: {role.Name} ({role.Id})" );
+	}
+
+	public void SetRole( string id )
+	{
+		SetRole( RoleHandler.Get( id ) );
+	}
+
+	public void SetupRole()
+	{
+		Log.Info( $"IsValid Role {Role} : {Role.IsValid()}" );
+
+		Transform transform = new( Vector3.Zero, Rotation.Identity );
+
+		if (Role.Check(GameRoles.Spectator))
+		{
+			Log.Info("ds");
+		} else if (Role.Check(GameRoles.Soccer))
+		{
+			Log.Info( "b" );
+		} else if ( Role.Check(GameRoles.Trashman))
+		{
+			Log.Info( "c" );
+		}
+
+		WorldPosition = transform.Position;
+		WorldRotation = transform.Rotation;
 	}
 }
