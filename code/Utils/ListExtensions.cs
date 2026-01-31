df6ae0a852fdbfd2b5a20326bcb26dace117512c
Titanovsky <titanovskyteam@gmail.com>
2026-01-31 07:17:49 +0500

    List.GetRandom()

---
+++ code/Utils/ListExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class ListExtensions
+{
+    extension<T>(List<T> list) where T : class
+    {
+        public T GetRandom()
+        {
+            if (list.Count == 0) return null;
+            if (list.Count == 1) return list[0];
+
+            return list[Random.Shared.Int(list.Count - 1)];
+        }
+    }
+}
